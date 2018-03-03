f25280ee1ad7d2dd86888054d38ae8c064dab135
Brooke Mortensen <brookeamymo@gmail.com>
2018-03-03 14:08:08 -0700

    able to add a button with input field

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 // set up array with objects we want to show up on the page load
 var topics = ["sad", "happy", "angry", "lonely", "overwhelmed"];
   // set up a function to add those buttons to the html
-  function addBtn() {
+  function addButton() {
     // empty to avoid repeat buttons
     $("#gifButtons").empty();
     // Loop through the topics array
@@ -12,10 +12,32 @@ var topics = ["sad", "happy", "angry", "lonely", "overwhelmed"];
       btn.addClass("emotion");
       btn.attr("data-name", topics[i]);
       btn.text(topics[i]);
-
       // place buttons on the page
       $("#gifButtons").append(btn);
     };
 };
-addBtn();
+// run the function
+addButton();
+
+  //create onclick when adding a custom button
+  $("#addNewButton").click(function(event) {
+    event.preventDefault();
+
+    // get new category from input box
+    var category = $("#newInput")
+      .val()
+      .trim();
+    // add new category to categoryList array
+    topics.push(category);
+
+    // calling the addButton function
+    addButton();
+
+    $("#input").focus();
+    console.log(category);
+  });
+
+  //call functions
+  addButton();
+  console.log("ok");
 });
